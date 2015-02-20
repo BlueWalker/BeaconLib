68c134ed90c87ef26464b918a92e43c0f8380e1c
acompagno <acompag@gmail.com>
2015-02-20 17:06:32 -0500

    Added framework for unittests

---
+++ src/test/java/walker/blue/beacon/lib/beacon/BluetoothDeviceToBeaconTest.java
@@ -0,0 +1,20 @@
+package walker.blue.beacon.lib.beacon;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.lang.System;
+
+/**
+ * Unit tests for BluetoothDeviceToBeacon class
+ */
+public class BluetoothDeviceToBeaconTest {
+
+    @Test
+    public void exampleTest() {
+        System.out.println("The test was run");
+        Assert.assertEquals(true, true);
+    }
+
+}
