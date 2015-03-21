c886a763b356381e9aefefc7f18dd3d95262b6d9
acompagno <acompag@gmail.com>
2015-03-21 00:45:25 -0400

    Added placeholder distance calculator

---
+++ src/main/java/walker/blue/beacon/lib/distance/DistanceCalculator.java
@@ -0,0 +1,19 @@
+package walker.blue.beacon.lib.distance;
+
+import walker.blue.beacon.lib.beacon.Beacon;
+
+/**
+ * Placeharder distance calculation class
+ */
+public class DistanceCalculator {
+
+    /**
+     * Calculate distance between the user and the given Beacon
+     *
+     * @param beacon Beacon whose distace will be calculated
+     * @return diastance between the given beacon and the user
+     */
+    public static double calculateDistance(final Beacon beacon) {
+        return -1.0f;
+    }
+}
